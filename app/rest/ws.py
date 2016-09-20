dbf5b8652d0a221c83bbca74a134bcb894a7e57d
Ishan Khare <ishankhare07@gmail.com>
2016-09-20 17:48:10 +0530

    fix handshake validator
    Signed-off-by: Ishan Khare <ishankhare07@gmail.com>

--- app/rest/ws.py
+++ app/rest/ws.py
@@ -8,13 +8,17 @@ class WsHandler(WebSocketHandler):
 
     def open(self, user_id):
         # make it int until handshake validator not completed
-        user_id = int(user_id)
-        WsHandler.connected[user_id] = self
-        self.user_id = user_id
-        self.write_message(json.dumps({
-            "type": "request",
-            "request": "user_id",
-            "user_id": user_id
+        result = HandshakeValidator().load({'user_id': user_id})
+        if result.errors:
+            print(result)
+            self.write_message(json.dumps(result.errors))
+            self.close()
+        else:
+            WsHandler.connected[result.data['user_id']] = self
+            self.user_id = result.data['user_id']
+            self.write_message(json.dumps({
+                "type": "handshake",
+                "user_id": result.data['user_id']
             }))
 
     def on_message(self, message):
@@ -37,8 +41,11 @@ class WsHandler(WebSocketHandler):
                 WsHandler.connected[reply.to_user].write_message(response)
             except KeyError as ke:
                 # receipent not yet connected
-                print("Receipent {0} not connected".format(reply.to_user)) 
+                print("Receipent {0} not connected".format(reply.to_user))
                 print(WsHandler.connected)
 
     def on_close(self):
-        WsHandler.connected.pop(self.user_id)
+        try:
+            WsHandler.connected.pop(self.user_id)
+        except KeyError as ke:
+            print('Client already removed or is not in connected')
