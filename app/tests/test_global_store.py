c6401e0d440e5a2578a5770d4d128b4ef74f14d7
Ishan Khare <ishankhare07@gmail.com>
2016-10-04 17:54:31 +0530

    add global store tests
    Signed-off-by: Ishan Khare <ishankhare07@gmail.com>

---
+++ app/tests/test_global_store.py
@@ -0,0 +1,46 @@
+from ..rest.global_store import *
+import unittest
+
+class GlobalStoreTestCase(unittest.TestCase):
+    def test_connected_property(self):
+        self.assertIsInstance(GlobalStore().connected, ModifiedDict)
+
+    def test_remove_connected(self):
+        gs = GlobalStore()
+        gs.connected[1] = 'one'
+        gs.connected[1] = 'two'
+        self.assertTrue(gs.remove_connected(1, 'one'))
+        self.assertTrue(gs.remove_connected(1, 'two'))
+        self.assertFalse(gs.remove_connected(1, 'one'))
+
+    def test_remove_verified(self):
+        gs = GlobalStore()
+        gs.verified[1] = 'one'
+        gs.verified[1] = 'two'
+        gs.remove_verified(1, 'one')
+        gs.remove_verified(1, 'two')
+        self.assertFalse(gs.verified[1])
+
+    def test_move_to_verified(self):
+        gs = GlobalStore()
+        gs.connected[1] = 'one'
+        gs.connected[1] = 'two'
+        gs.move_to_verified(1, 'one')
+        self.assertEqual(gs.connected[1], ['two'])
+        self.assertIsNotNone(gs.verified[1])
+
+    def tearDown(self):
+        gs = GlobalStore()
+        gs.reset()
+
+
+class ModifiedDictTestCase(unittest.TestCase):
+    def test_get(self):
+        md = ModifiedDict()
+        md[1] = 'one'
+        self.assertIsNotNone(md.get(1))
+
+    def test_repr(self):
+        md = ModifiedDict()
+        md[1] = 'one'
+        self.assertTrue(str(md))
